6194c3fda34b3aa734205b0d45c3c3127634c8ed
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-27 14:54:47 +0900

    バリデーション のテストコード作成

--- spec/models/task.rb
+++ spec/models/task.rb
@@ -1,18 +1,14 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :model do
-  it 'task_namega空白ならバリデーションが通らない' do
-    task = Task.new(name: '', description: '通らない')
-    expect(task).not_to be_valid
-    
-  end
-  it 'descriptionが空白ならバリデーションが通らない' do
-    task = Task.new(name: '通らない', description: '')
-    expect(task).not_to be_valid
-  end
 
 
-  it 'nameとdescriptionに内容が記載されていればバリデーションが通る' do
-    task = Task.new(name: 'バリデーション にクリアする', description: 'バリデーション にクリアする')
-    expect(task).to be_valid
+  context '新規作成画面でタスク名を入れなかったとき' do
+    let(:task_name) {''}
+
+    it 'バリデーション が働いてエラーになる' do
+      whithin '#error_explanation' do
+        expect(page).to have_content '名称を入力してください'
+      end 
+    end
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,6 +1,30 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
 
+  before do
+    # 「タスク一覧画面」や「タスク詳細画面」などそれぞれのテストケースで、before内のコードが実行される
+    # 各テストで使用するタスクを1件作成する
+    # 作成したタスクオブジェクトを各テストケースで呼び出せるようにインスタンス変数に代入
+    @task = FactoryBot.create(:task, task_name: 'task')
+  end
+  describe 'タスク一覧画面' do
+    context 'タスクを作成した場合' do
+      it '作成済みのタスクが表示される'
+        # beforeに必要なタスクデータが作成されるので、ここでテストデータ作成処理を書く必要がない
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
+    context '複数のタスクを作成した場合' do
+      it 'タスクが作成日時の降順に並んでいる' do
+        new_task = FactoryBot.create(:task, task_name: 'new_task')
+        visit tasks_path
+        task_list = all('.task_row') # タスク一覧を配列として取得するため、View側でidを振っておく
+        expect(task_list[0]).to have_content 'new_task'
+        expect(task_list[1]).to have_content 'task'
+      end
+    end
+
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
       it '作成済みのタスクが表示される' do
