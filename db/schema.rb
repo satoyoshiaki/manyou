2c5d290838e6f0e160831b884f04af83a7918d56
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-28 17:13:25 +0900

    カラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_064347) do
+ActiveRecord::Schema.define(version: 2020_04_28_075533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_04_23_064347) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline"
+    t.integer "status"
+    t.integer "priority"
   end
 
 end
