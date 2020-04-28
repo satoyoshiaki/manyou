bf567eda90d0303064e857e07d1dd0edc0ac6a10
佐藤良昭 <yoshiprograming@gmail.com>
2020-04-28 17:52:18 +0900

    viewにカラム追加とモデルでプルタブの調整

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
     validates :task_name, presence: true
     validates :description, presence: true
+
+    enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
+    enum priority: { 高: 0, 中: 1, 低: 2 }
 end
