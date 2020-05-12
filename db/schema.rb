e33f2c6294790474e0bb4ffdecad19c0b3b12278
佐藤良昭 <yoshiprograming@gmail.com>
2020-05-12 17:19:39 +0900

    ラベルのmvc作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_084019) do
+ActiveRecord::Schema.define(version: 2020_05_12_080657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labels", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "task_name"
