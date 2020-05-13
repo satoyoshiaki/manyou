7159188b05a0184e98c28ec94b377dd50c37db26
佐藤良昭 <yoshiprograming@gmail.com>
2020-05-13 10:58:22 +0900

    ラベルのseedデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 User.create!(name:  "管理者",
-             id: 100,
-             email: "admin@g.cact",
+             id: 108,
+             email: "admin@gd.cact",
              password:  "adminadmin",
              password_confirmation: "adminadmin",
              admin: true)
 
+
+Label.create(name: "taskラベル８")
+Label.create(name: "taskラベル9")
+Label.create(name: "taskラベ0")
+Label.create(name: "taskラベル11")
+Label.create(name: "taskラベル4")
